df83353156c0e2c4af3a751a84a4116ad08faf97
josh_scriptz <codewithjoshh@gmail.com>
2025-08-29 04:23:08 +0100

    j

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             {
                 userAgent: '*',
                 allow: '/',
-                disallow: '/admin',
+                // disallow: '/admin',
                 crawlDelay: 2
             }
         ],
